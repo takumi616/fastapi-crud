29b914c24270c7a3851a5d5ac54e736fa601ef73
takumi616 <ohohyeah321@icloud.com>
2026-01-12 10:08:38 +0900

    Modify the type of returned value

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from app.db.session import SessionDep
 from app.models.user import User
 from app.schemas.user import UserCreateRequest
@@ -6,11 +7,6 @@ from app.schemas.user import UserCreateRequest
 async def insert_new_user(session: SessionDep, req: UserCreateRequest) -> User:
     user = User(**req.model_dump())
     session.add(user)
-
     await session.commit()
     await session.refresh(user)
-
     return user
-    
-
-    
