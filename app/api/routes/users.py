ee8941903e0d8c78626711fa76f1f620cdf5c8e4
takumi616 <ohohyeah321@icloud.com>
2026-01-12 10:49:27 +0900

    Modify the type of returned value and remove model validate method

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -8,9 +8,8 @@ from app.schemas.user import UserCreateRequest, UserResponse
 router = APIRouter(prefix="/users", tags=["users"])
 
 @router.post("", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-async def create_new_user(session: SessionDep, req: UserCreateRequest) -> UserResponse:
-    user = await user_crud.insert_new_user(session, req)
-    return UserResponse.model_validate(user)
+async def create_new_user(session: SessionDep, req: UserCreateRequest) -> User:
+    return await user_crud.insert_new_user(session, req)
 
 @router.get("", response_model=list[UserResponse], status_code=status.HTTP_200_OK)
 async def get_user_list(session: SessionDep, skip: int = 0, limit: int = 100) -> list[User]:
